e5dceb30514bbde88473c1728907610ceb75a1ba
Tomás Gonçalves <tomasfgon@gmail.com>
2020-05-18 17:52:48 +0100

    push

---
+++ cmake-build-debug/CMakeFiles/Projeto.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projeto.dir/main.cpp.o"
+  "Projeto"
+  "Projeto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Projeto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

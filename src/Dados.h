4f7aa4a686abf1342aa505f293a79ae993fe0ccc
MiNeves00 <61327611+MiNeves00@users.noreply.github.com>
2020-05-20 20:12:15 +0100

    added constructors

--- src/Dados.h
+++ src/Dados.h
@@ -13,35 +13,74 @@
 
 using namespace std;
 
+int idNumber;
+
 class VerticeInfo {
+public:
+
+    VerticeInfo(Coordenadas &coordenadas) : coordenadas(coordenadas) {
+        id = idNumber;
+        idNumber++;
+    }
+
+private:
+
     int id;
     Coordenadas coordenadas;
+
+
 };
+
 
 //Dados Entrada
 
 class TransporteUE {
 
+public:
+    TransporteUE(TipoLixo &tipoLixo1, double &pesoMin1, double &pesoMax1) : tipoLixo(tipoLixo1), pesoMin(pesoMin1), pesoMax(pesoMax1) {}
+
+private:
     TipoLixo tipoLixo;
     double pesoMin;
     double pesoMax;
+
+
 };
+
 
 class DepositoUP {
 
+public:
+    DepositoUP(TipoLixo &tipoLixo1, double &peso1) : tipoLixo(tipoLixo1), peso(peso1) {}
+private:
     TipoLixo tipoLixo;
     double peso;
+
+
 };
 
 
 
 class PontoRecolhaDomiciliario : VerticeInfo{
 
+public:
+    PontoRecolhaDomiciliario(Coordenadas &coordenadas, vector<TipoLixo> &tipoLixo) : VerticeInfo(
+            coordenadas), tipoLixo(tipoLixo) {}
+
+private:
+
     vector<TipoLixo>  tipoLixo;
     vector<double> pesos;
 };
 
+
+
 class PontoRecolha : VerticeInfo {
+public:
+    PontoRecolha(Coordenadas &coordenadas, vector<TipoLixo> &tipoLixo) : VerticeInfo(coordenadas),
+                                                                               tipoLixo(tipoLixo) {}
+
+private:
 
     vector<TipoLixo>  tipoLixo;
     vector<double> taxasOcupacao;
@@ -49,15 +88,25 @@ class PontoRecolha : VerticeInfo {
 };
 
 class PontoPartida : VerticeInfo {
+public:
+    PontoPartida(Coordenadas &coordenadas) : VerticeInfo(coordenadas) {}
 
 };
 
 class CentroReciclagem : VerticeInfo {
+public:
+    CentroReciclagem(Coordenadas &coordenadas, TipoLixo tipoLixo) : VerticeInfo(coordenadas), tipoLixo(tipoLixo) {}
+
+private:
 
     TipoLixo tipoLixo;
 };
 
 class Camiao {
+public:
+    Camiao(TipoLixo tipoLixo) : tipoLixo(tipoLixo) {}
+
+private:
 
     TipoLixo tipoLixo;
     double capacidadeAtual;
@@ -69,6 +118,10 @@ class Camiao {
 //Dados Saida
 
 class SequenciaIdeal {
+public:
+    SequenciaIdeal(vector<Edge<VerticeInfo>> &arestasIdeais) : arestasIdeais(arestasIdeais) {}
+
+private:
 
     vector<Edge<VerticeInfo>> arestasIdeais;
 };

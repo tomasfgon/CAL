d7b8c4379173d2c510feccc45b4909414a410837
Tomás Gonçalves <tomasfgon@gmail.com>
2020-05-22 15:32:54 +0100

    filereader (espero ter conseguido ignorar os icons agora)

--- src/fileReader.h
+++ src/fileReader.h
@@ -2,13 +2,29 @@
 // Created by Tomas Goncalves on 21/05/2020.
 //
 
-#ifndef PROJETO_FILEREADER_H
-#define PROJETO_FILEREADER_H
+#pragma once
 
 
-class fileReader {
+
+
+#include "Dados.h"
+#include <string>
+#include <fstream>
+#include <math.h>
+
+class FileReader {
+
+
+public:
+
+    //template<class T>
+    void readNodes_simples(Graph<VerticeInfo> &graph, string nome);
+
+    //template<class T>
+    void readEdges_simples(Graph<VerticeInfo> &graph, string nome);
+
 
 };
 
 
-#endif //PROJETO_FILEREADER_H
+
